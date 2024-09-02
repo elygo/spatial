683834ea24fef905e702ba4451c9aa3a7074d85a
elygo <farmonov.en@gmail.com>
2024-09-02 17:18:36 +0500

    init: add files to spatial_app

--- src/test/java/uz/location/spatial/SpatialApplicationTests.java
+++ src/test/java/uz/location/spatial/SpatialApplicationTests.java
@@ -1,6 +1,5 @@
 package uz.location.spatial;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
